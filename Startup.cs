30354a3fc3e96c95ab773fc1d2e7790ab9669c44
sidewinder040 <macevic040@hotmail.co.uk>
2017-08-28 19:46:07 +0100

    Added MvcMovie Model and Context Add EF Core - Builds OK - Ready to Scaffold.

--- Startup.cs
+++ Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using MvcMovie.Models;
 
 namespace MvcMovie
 {
@@ -22,6 +24,8 @@ namespace MvcMovie
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+            services.AddDbContext<MvcMovieContext>(options => 
+                options.UseSqlite("Data Source=MvcMovie.db"));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
